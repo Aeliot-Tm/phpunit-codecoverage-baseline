38fa9492d683e879747aea5fc6dcc5f5318f507f
Anatoliy Melnikov <5785276@gmail.com>
2026-06-16 22:44:21 +0300

    Implement preparing of release with PHAR file

---
+++ src/Console/Application.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the PHPUnit code coverage baseline project.
+ *
+ * (c) Anatoliy Melnikov <5785276@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Aeliot\PHPUnitCodeCoverageBaseline\Console;
+
+use Symfony\Component\Console\Application as SymfonyApplication;
+
+final class Application extends SymfonyApplication
+{
+    public const VERSION = '2.1.0';
+
+    public function __construct()
+    {
+        parent::__construct('PHPUnit code coverage baseline', self::VERSION);
+    }
+}
